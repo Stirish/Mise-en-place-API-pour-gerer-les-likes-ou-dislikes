969577570b2f88463264cf96fab5e82a58ead637
Stirish <floo.swartzy@hotmail.fr>
2022-04-12 15:27:54 +0200

    Correction final

--- backend/routes/like.js
+++ backend/routes/like.js
@@ -1,14 +1,10 @@
-//Importation de express
-const express = require('express');
-//Mettre express et le lier avec la fonction router dans une constante
-const router = express.Router();
-
-const likeCtrl = require('../controllers/like')
-//Importer /middleware/auth.j dans une constante
-const auth = require('../middleware/auth');
+const express = require('express'); //On importe express
+const router = express.Router(); //Lier la fonction router à express dans une constante
+const likeCtrl = require('../controllers/like')//On importe /controllers/like dans une constante
+const auth = require('../middleware/auth'); //On importe /middleware/auth.j dans une constante
 
 //Route pour l'option like dislike
-router.post('/:id/like', auth, likeCtrl.handleLikeOption)
+router.post('/:id/like', auth, likeCtrl.handleLikeOption);
 
 //Exporter les routes
 module.exports = router;
